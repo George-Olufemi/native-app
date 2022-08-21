43692780249bc2baae8c39856aaf9be271ced32d
George Olufemi <georgeoluolufemi@gmail.com>
2022-08-22 00:27:49 +0100

    v1 commit

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('Open up App.js to start working on your app!');
+  const [inputText, setInputText] = useState('');
+  const changeTextHandler = () => {
+    setText('Hello World');
+  }
+  const changeinput = () => {
+    setInputText(inputText)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
       <StatusBar style="auto" />
+      <View style={styles.navBar}>
+        <Text>Hello World</Text>
+      </View>
+      <Button onPress={changeTextHandler} title = "touch" />
+      <TextInput 
+        style={styles.input} 
+        placeholder="Enter your name" 
+        placeholderTextColor='white' 
+        onChangeText={setInputText}
+      />
+      <Button onPress={changeinput} title = "Change Text" />
+      <Text>{inputText}</Text>
     </View>
   );
 }
@@ -17,4 +38,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  navBar: {
+    backgroundColor: 'red',
+    height: 100,
+    width: '100%',
+    position: "absolute",
+    bottom: 0
+  },
+  input: {
+    width: 300,
+    color: "white",
+    backgroundColor: 'blue',
+    padding: 5,
+    placeholder: 'white',
+  }
 });
